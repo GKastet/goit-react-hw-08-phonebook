b856570b0ab1fe87b0e2833e336b0788500dec25
GKastet <127600582+GKastet@users.noreply.github.com>
2023-12-26 12:02:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,27 +7,27 @@ import store, { persistor } from 'redux/store';
 import App from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
-  //</React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   //<React.StrictMode>
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </PersistGate>
+//   </Provider>
+//   //</React.StrictMode>
+// );
 
 //GitHub
-// ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-  // <Provider store={store}>
-  //   <PersistGate loading={null} persistor={persistor}>
-  //     <BrowserRouter basename="/goit-react-hw-08-phonebook">
-  //       <App />
-  //     </BrowserRouter>
-  //   </PersistGate>
-  // </Provider>
-  //</React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  // </React.StrictMode>
+);
